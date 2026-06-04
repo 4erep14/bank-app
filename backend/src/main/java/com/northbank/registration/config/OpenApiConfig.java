8df7004538e5bc4284e11e4cc321918106551fcb
Bohdan Bazyl <b.bazyl.r@gmail.com>
2026-06-04 11:35:57 +0300

    docs: add swagger bearer auth config

---
+++ backend/src/main/java/com/northbank/registration/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+// Story: API documentation
+package com.northbank.registration.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI northBankOpenApi() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("NorthBank API")
+                .description("NorthBank digital banking backend API")
+                .version("v1"))
+            .components(new Components()
+                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")));
+    }
+}
